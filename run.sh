50fed3980a1d5ff5acf12986d113a5bd6b453924
vakandi <wael.bousfira@protonmail.com>
2023-03-13 17:00:03 +0100

    update

--- run.sh
+++ run.sh
@@ -1,14 +1,36 @@
 #!/bin/sh
 pid="52427"
 ./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+
 ./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+
 ./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+
 ./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+
+
 ./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+
+
 ./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+
+
+
 ./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀" &
+
+
+
