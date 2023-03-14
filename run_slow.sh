19e5e9625b157098f0f445626cfadaf6ecc85379
wbousfi <wael.bousfira@protonmail.com>
2023-03-14 21:31:02 +0100

    update9668

--- run_slow.sh
+++ run_slow.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-pid="52427"
+pid="16433"
 cp client client2
 cp client client3
+
+./client $pid " \
+	"
 
 ./client $pid "gsw$RANDOMsedhse    random : $RANDOM    😀"
 ./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM    😀"
