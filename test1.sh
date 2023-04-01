0c0fc0bf78564068f7d9dc72ba2989547897622e
vakandi <wael.bousfira@protonmail.com>
2023-04-01 20:45:13 +0000

    update

---
+++ test1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sleep 3s
+pid="$(ps -ax | grep "./server" | head -n 1 | awk '{ print $1 }')"
+emoji="🕵️"
+loop=15
+var=1
+./client $pid " \
+	"
+
+while [ $var -le $loop ]
+do
+	./client $pid " Test random number ::::: $RANDOM Test emoji:: $emoji"
+	./client $pid " Test random number ::::: $RANDOM Test emoji:: $emoji"
+	./client $pid " Test random number ::::: $RANDOM Test emoji:: $emoji"
+	./client $pid " ::::::::::::      Loops $var done  ::::::  :::::: "
+	var=$((var+1))
+done
+rm nohup.out
--- test_one_thread.sh
+++ test_one_thread.sh
@@ -3,38 +3,15 @@ pid="$(ps -ax | grep "./server" | head -n 1 | awk '{ print $1 }')"
 emoji="🕵️"
 
 rm client
-rm client2
-rm client3
 rm server
 gcc -Wall -Wextra -Werror server.c -o server 
 gcc -Wall -Wextra -Werror client.c -o client 
-cp client client2
-cp client client3
 
-./client $pid " \
-	"
-
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    $emoji"
-./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    $emoji"
-./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    $emoji"
-./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    $emoji"
-./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    $emoji"
-./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-
-./client $pid "gsw$RANDOMsedhse    random : $RANDOM    $emoji"
-./client2 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-./client3 $pid "gsw$RANDOMsedhse    random : $RANDOM   $emoji"
-
+echo "Compilation and preparation done..."
+echo "Starting the tests.."
+echo "\n"
+sh /root/code/minitalk/test1.sh &
+echo "the tester is running"
+clear
+echo "THE PID OR THE SERVER :::::::: "
+./server
