b59b4ee4ebf40f4723660f350e28cad6beb6f878
wbousfi <wael.bousfira@protonmail.com>
2023-04-11 20:55:05 +0000

    update7720

--- client.c
+++ client.c
@@ -1,36 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wbousfir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/11 20:50:55 by wbousfir          #+#    #+#             */
+/*   Updated: 2023/04/11 20:50:57 by wbousfir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
 
-int     ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-        int     x;
-        int     a;
-        int     y;
+	int	x;
+	int	a;
+	int	y;
 
-        y = 1;
-        a = 0;
-        x = 0;
-        while ((str[x] >= 9 && str[x] <= 13) || str[x] == 32)
-                x++;
-        if (str[x] == '-' || str[x] == '+')
-        {
-                if (str[x] == '-')
-                        y = -1;
-                x++;
-        }
-        while (str[x] >= '0' && str[x] <= '9')
-        {
-                a = a * 10 + (str[x] - 48);
-                x++;
-        }
-        return (a * y);
+	y = 1;
+	a = 0;
+	x = 0;
+	while ((str[x] >= 9 && str[x] <= 13) || str[x] == 32)
+		x++;
+	if (str[x] == '-' || str[x] == '+')
+	{
+		if (str[x] == '-')
+			y = -1;
+		x++;
+	}
+	while (str[x] >= '0' && str[x] <= '9')
+	{
+		a = a * 10 + (str[x] - 48);
+		x++;
+	}
+	return (a * y);
 }
-
 
 void	send_signal(int c, int pid)
 {
-	int bit;
+	int	bit;
 
 	bit = 0;
 	while (bit < 8)
@@ -39,28 +50,26 @@ void	send_signal(int c, int pid)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		bit++;
 		usleep(500);
+		bit++;
 	}
-	usleep(500);
 }
 
 int	main(int argc, char **argv)
 {
-	int x;
+	int	x;
 
 	x = 0;
 	if (argc == 3)
 	{
-		while(argv[2][x] != '\0')
+		while (argv[2][x] != '\0')
 		{
 			send_signal((int)argv[2][x], ft_atoi(argv[1]));
 			x++;
 		}
-	send_signal('\n', ft_atoi(argv[1]));
+		send_signal('\n', ft_atoi(argv[1]));
 	}
 	else
 		write(1, "Failed not enough argument", 26);
-		
-	return(0);
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wbousfir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/11 20:54:29 by wbousfir          #+#    #+#             */
+/*   Updated: 2023/04/11 20:54:30 by wbousfir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
@@ -7,65 +19,64 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void    ft_putnbr(int n)
+void	ft_putnbr(int n)
 {
-        if (n == -2147483648)
-                write(1, "-2147483648", 10);
-        else
-        {
-                if (n < 0)
-                {
-                        ft_putchar('-');
-                        n = n * -1;
-                }
-                if (n > 9)
-                {
-                        ft_putnbr(n / 10);
-                }
-                ft_putchar((n % 10) + '0');
-        }
+	if (n == -2147483648)
+		write(1, "-2147483648", 10);
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar('-');
+			n = n * -1;
+		}
+		if (n > 9)
+		{
+			ft_putnbr(n / 10);
+		}
+		ft_putchar((n % 10) + '0');
+	}
 }
 
-
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-        size_t  b;
+	size_t	b;
 
-        b = 0;
-        while (s[b] != '\0')
-        	b++;
-        return (b);
+	b = 0;
+	while (s[b] != '\0')
+		b++;
+	return (b);
 }
 
 void	receive_signal(int signal)
 {
-	static int max_bytes;
-	static char c;
+	static int	max_bytes;
+	static char	c;
+
 	if (signal == SIGUSR1)
-       		c = c | (0x01 << max_bytes);
+		c = c | (0x01 << max_bytes);
 	max_bytes++;
 	if (max_bytes == 8)
 	{
-		//printf("%c", c);
 		write(1, &c, 1);
 		max_bytes = 0;
 		c = 0;
 	}
 }
 
-
 int	main(int argc, char **argv)
 {
 	*argv = 0;
-	
 	if (argc > 1)
 	{
 		write(1, "Error Don't put any arguments\n", 10);
-		return(0);
+		return (0);
 	}
 	ft_putnbr(getpid());
 	signal(SIGUSR1, receive_signal);
 	signal(SIGUSR2, receive_signal);
-	while(1);
-	return(0);
+	while (1)
+	{
+	}
+	return (0);
 }
